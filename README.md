95ca9e277f9fb960ba3a1e79ea5094afdd7500d8
Nazman Gena <nazmang@gmail.com>
2019-08-23 22:38:59 +0300

    Added proper build.sh

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# jenkins
-aarch64 jenkins
+# Jenkins Docker image for ARM
+The Jenkins Continuous Integration and Delivery server [available on Docker Hub](https://hub.docker.com/nazman/jenkins-armhf)
+
+This is a fully functional Jenkins server based on 
+[https://github.com/jenkinsci/docker](https://github.com/jenkinsci/docker).
+
+<img src="https://jenkins.io/sites/default/files/jenkins_logo.png"/>
+Build your own image using __build.sh__ script or use this one
+```
+docker run -d -v jenkins_home:/var/jenkins_home -p 8080:8080 -p 50000:50000 nazman/jenkins-armhf
+```
---
+++ build.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+JENKINS_VERSION=2.164.1
+JENKINS_USER=jenkins
+JENKINS_GROUP=jenkins
+JENKINS_UID=1000
+JENKINS_GID=1000
+JENKINS_HOME=/var/jenkins_home
+JENKINS_HTTP_PORT=8080
+JENKINS_AGENT_PORT=50000
+JENKINS_SHA=65543f5632ee54344f3351b34b305702df12393b3196a95c3771ddb3819b220b
+JENKINS_URL=https://repo.jenkins-ci.org/public/org/jenkins-ci/main/jenkins-war/${JENKINS_VERSION}/jenkins-war-${JENKINS_VERSION}.war
+TINI_VERSION=v0.16.1
+
+PREFIX="nazmang"
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+RESOURCEDIR="$BASEDIR/docker"
+ARCH="$(dpkg --print-architecture)"
+
+if [ ! -d "$RESOURCEDIR" ]; then
+	  mkdir $RESOURCEDIR && cd $RESOURCEDIR
+	  curl -sSLO https://raw.githubusercontent.com/jenkinsci/docker/master/init.groovy
+	  curl -sSLO https://raw.githubusercontent.com/jenkinsci/docker/master/jenkins.sh
+	  curl -sSLO https://raw.githubusercontent.com/jenkinsci/docker/master/install-plugins.sh
+	  curl -sSLO https://raw.githubusercontent.com/jenkinsci/docker/master/plugins.sh
+	  curl -sSLO https://raw.githubusercontent.com/jenkinsci/docker/master/jenkins-support
+	  curl -sSLO https://raw.githubusercontent.com/jenkinsci/docker/master/tini_pub.gpg
+	  curl -sSLO https://raw.githubusercontent.com/jenkinsci/docker/master/tini-shim.sh
+	  chmod +rx *
+	  cd $BASEDIR
+fi
+
+docker build \
+	--rm --tag $PREFIX/jenkins-armhf:$JENKINS_VERSION \
+	--build-arg JENKINS_VERSION=$JENKINS_VERSION \
+	--build-arg user=$JENKINS_USER \
+	--build-arg group=$JENKINS_GROUP \
+	--build-arg uid=$JENKINS_UID \
+	--build-arg gid=$JENKINS_GID \
+	--build-arg JENKINS_HOME=$JENKINS_HOME \
+	--build-arg http_port=$JENKINS_HTTP_PORT \
+	--build-arg agent_port=$JENKINS_AGENT_PORT \
+	--build-arg JENKINS_SHA=$JENKINS_SHA \
+	--build-arg JENKINS_URL=$JENKINS_URL \
+	--build-arg TINI_VERSION=$TINI_VERSION \
+	--no-cache --file Dockerfile.$ARCH .
+	
+docker tag $PREFIX/jenkins-armhf:$JENKINS_VERSION $PREFIX/jenkins-armhf:latest
+#docker push $PREFIX/jenkins-armhf:$JENKINS_VERSION
+#docker push $PREFIX/jenkins-armhf:latest
